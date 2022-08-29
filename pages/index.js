cf6dda219a01293042cd39eca51dbc331c9e30b2
Declan Nnadozie <zedonbiz@gmail.com>
2022-08-29 16:35:44 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -146,51 +146,17 @@ export default function Home() {
       </main>
       {tutors.length === 0 ? null : (
         <div ref={ref} className="bg-white">
-          <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
-            <div className="space-y-12">
-              {/* <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">Meet our leadership</h2> */}
-
-              <ul
-                role="list"
-                className="space-y-12 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 lg:gap-y-12 lg:space-y-0"
-              >
-                {tutors.map((person, i) => (
-                  <li key={i}>
-                    <div className="space-y-4 sm:grid sm:grid-cols-3 sm:gap-6 sm:space-y-0 lg:gap-8">
-                      <div className="h-0 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-                        <img
-                          className="object-cover shadow-lg rounded-lg"
-                          src={person.tutor.photo}
-                          alt=""
-                        />
-                      </div>
-                      <div className="sm:col-span-2">
-                        <div className="space-y-4">
-                          <div className="text-lg leading-6 font-medium space-y-1">
-                            <h3>
-                              {person.tutor.first_name} {person.tutor.last_name}
-                            </h3>
-                            <p className="text-indigo-600">
-                              {person.tutor.email}
-                            </p>
-                            <p className="text-indigo-600">
-                              {person.tutor.phone}
-                            </p>
-                            <p className="text-indigo-600">{person.role}</p>
-                          </div>
-                          <div className="text-lg">
-                            <p className="text-xl font-bold">Subjects</p>
-                            {/* <p className="text-gray-500">
-                              {person.tutor.subjects}
-                            </p> */}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+          <div className="mx-auto grid lg:grid-cols-3 gap-8 py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+            {tutors.map(people => (<div className="h-32 overflow-clip rounded-lg border border-indigo-500 flex">
+              <figure className="w-32 overflow-clip h-full">
+                <img src={people?.tutor?.photo} className="w-full h-full overflow-clip object-fill"/>
+              </figure>
+              <div className="p-6">
+                <p className="text-xl text-indigo-500 font-bold">{people.tutor.first_name} {people.tutor.last_name}</p>
+                <p className="text-sm text-gray-500 font-bold">{people.tutor.email}</p>
+                <p className="text-sm text-gray-500 font-bold">{people.tutor.phone}</p>
+              </div>
+            </div>))}
           </div>
         </div>
       )}
