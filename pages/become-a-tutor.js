f592be9ae4fc857f191bfaac48d8d62977f4171a
Declan Nnadozie <zedonbiz@gmail.com>
2022-09-17 17:22:54 +0100

    update

--- pages/become-a-tutor.js
+++ pages/become-a-tutor.js
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import { API_URL } from '../config'
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { API_URL } from "../config";
 // import { Switch } from '@headlessui/react'
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const INITIAL_STATE = {
-    first_name: "",
-    last_name: "",
-    email: "",
-    zip : "",
-    subject: "",
-    mobile_phone: "",
-    address: ""
-}
+  first_name: "",
+  last_name: "",
+  email: "",
+  zip: "",
+  subject: "",
+  mobile_phone: "",
+  address: "",
+};
 export default function BecomeTutor() {
-
-  const [state, setState] = useState(INITIAL_STATE)
+  const [loading, setLoading] = useState(false);
+  const [state, setState] = useState(INITIAL_STATE);
 
   return (
     <div className="overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8 lg:py-24">
@@ -39,10 +40,21 @@ export default function BecomeTutor() {
               height={20}
               patternUnits="userSpaceOnUse"
             >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
             </pattern>
           </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
         </svg>
         <svg
           className="absolute right-full bottom-0 -translate-x-1/2 transform"
@@ -61,42 +73,69 @@ export default function BecomeTutor() {
               height={20}
               patternUnits="userSpaceOnUse"
             >
-              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
             </pattern>
           </defs>
-          <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
         </svg>
         <div className="text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Become a tutor
+          </h2>
           <p className="mt-4 text-lg leading-6 text-gray-500">
-            Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus
-            arcu.
+            {/* Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat
+            massa dictumst amet. Sapien tortor lacus arcu. */}
           </p>
         </div>
         <div className="mt-12">
-          <form onSubmit={e => {
-            e.preventDefault()
-            fetch(`${API_URL}/tutors`, {
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setLoading(true)
+              fetch(`${API_URL}/tutors`, {
                 method: "POST",
                 headers: {
-                    "Content-Type":"application/json"
+                  "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    data: {
-                        ...state
-                    }
-                })
-            })
-          }} className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                  data: {
+                    ...state,
+                  },
+                }),
+              }).then(resp => {
+                if(resp.ok) toast.success("Successfull")
+                else toast.error("Action Failed")
+              }).catch(err => {
+                toast.error("Error")
+              }).finally(() => {
+                setLoading(false)
+              });
+            }}
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
             <div>
-              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-gray-700"
+              >
                 First name
               </label>
               <div className="mt-1">
                 <input
                   type="text"
-                  onChange={e => {
-                    setState({...state, first_name: e.target.value})
+                  onChange={(e) => {
+                    setState({ ...state, first_name: e.target.value });
                   }}
                   value={state.first_name}
                   name="first-name"
@@ -107,14 +146,17 @@ export default function BecomeTutor() {
               </div>
             </div>
             <div>
-              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Last name
               </label>
               <div className="mt-1">
                 <input
-                onChange={e => {
-                    setState({...state, last_name: e.target.value})
-                }}
+                  onChange={(e) => {
+                    setState({ ...state, last_name: e.target.value });
+                  }}
                   type="text"
                   name="last-name"
                   id="last-name"
@@ -125,35 +167,41 @@ export default function BecomeTutor() {
             </div>
 
             <div>
-              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Email
               </label>
               <div className="mt-1">
                 <input
-                onChange={e => {
-                    setState({...state, email: e.target.value})
-                }}
+                  onChange={(e) => {
+                    setState({ ...state, email: e.target.value });
+                  }}
                   type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
                   className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                 />
               </div>
             </div>
             <div>
-              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="zip"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Postal/Zip Code
               </label>
               <div className="mt-1">
                 <input
-                onChange={e => {
-                    setState({...state, zip: e.target.value})
-                }}
+                  onChange={(e) => {
+                    setState({ ...state, zip: e.target.value });
+                  }}
                   type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
+                  name="zip"
+                  id="zip"
+                  autoComplete="zip"
                   className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                 />
               </div>
@@ -175,61 +223,72 @@ export default function BecomeTutor() {
             </div> */}
 
             <div>
-              <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Phone
               </label>
               <div className="mt-1">
                 <input
-                onChange={e => {
-                    setState({...state, mobile_phone: e.target.value})
-                }}
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
+                  onChange={(e) => {
+                    setState({ ...state, mobile_phone: e.target.value });
+                  }}
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  autoComplete="phone"
                   className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                 />
               </div>
             </div>
 
             <div>
-              <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="address"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Address
               </label>
               <div className="mt-1">
                 <input
-                onChange={e => {
-                    setState({...state, address: e.target.value})
-                }}
+                  onChange={(e) => {
+                    setState({ ...state, address: e.target.value });
+                  }}
                   type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
+                  name="address"
+                  id="address"
+                  autoComplete="address"
                   className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Subjects
               </label>
               <div className="mt-1">
                 <textarea
-                onChange={e => {
-                    setState({...state, subject: e.target.value})
-                }}
+                  onChange={(e) => {
+                    setState({ ...state, subject: e.target.value });
+                  }}
                   id="message"
                   name="message"
                   rows={4}
+                  placeholder="e.g Grade 9 Chemistry, Grade 8 Physics"
                   className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                  defaultValue={''}
+                  defaultValue={""}
                 />
               </div>
             </div>
-           
+
             <div className="sm:col-span-2">
               <button
+              disabled={loading}
                 type="submit"
                 className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
@@ -240,5 +299,5 @@ export default function BecomeTutor() {
         </div>
       </div>
     </div>
-  )
+  );
 }
