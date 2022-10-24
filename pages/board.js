30620b4958c6faf583fc5f8a5e4d38d21cd954e7
Declan Nnadozie <zedonbiz@gmail.com>
2022-10-24 19:00:39 +0100

    update for tutor

--- pages/board.js
+++ pages/board.js
@@ -2,13 +2,23 @@ import { data } from "autoprefixer";
 import { useEffect, useState } from "react";
 import JobBoardTable from "../components/Table";
 import { API_URL } from "../config";
+import qs from "qs"
 
 export default function Board() {
 
     let [jobs, setJob] = useState([])
     useEffect(() => {
         let task = async () => {
-            let resp = await fetch(`${API_URL}/jobs?populate=*`)
+            let query = qs.stringify({
+                filters: {
+                    active: {
+                        $eq: true
+                    }
+                },
+
+                populate: "*"
+            })
+            let resp = await fetch(`${API_URL}/jobs?${query}`)
             if(resp.ok){
                 let data = await resp.json()
                 setJob(data.data)
